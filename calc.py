8cb22185562a3ee6e41fef1841c17c0c75a7a877
cyxilef <157657991+cyxilef@users.noreply.github.com>
2024-02-05 16:27:10 -0800

    Calculator Lab

--- calc.py
+++ calc.py
@@ -1,1 +1,31 @@
+num1 = int(input("Enter your num 1: "))
+op = input("Enter your operator: ")
+num2 = int(input("Enter your num2: "))
+
+if op == "+":
+		total = num1 + num2
+		print(f"{num1} + {num2} = {total}")
+
+elif op == "-":
+		print(f"{num1} - {num2} = {num1 - num2}")
+
+elif op == "*":
+		print(f"{num1} * {num2} = {num1 * num2}")
+
+elif op == "/":
+		print(f"{num1} / {num2} = {num1 / num2}")
+
+elif op == "**":
+		print(f"{num1} ** {num2} = {num1 ** num2}")
+
+elif op == "//":
+		print(f"{num1} // {num2} = {num1 // num2}")
+
+elif op == "%":
+		print(f"{num1} % {num2} = {num1 % num2}")
+
+
+
+
+
 
